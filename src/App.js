40f4f4529781d3a3d6cfad002018d42c8dc15d0b
Lukas Lee <Lukas.Lee@emishealth.com>
2019-07-16 14:43:42 +0100

    component update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import NameElement from './nameElement';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state  = {
+    firstName: 'Harper',
+    lastName: 'Perez'
+  };
+
+
+  render(){
+    return (
+      <div className="App">
+        <p>Romain Bardet</p>
+        <NameElement name={this.state} />
+      </div>
+    )
+  }
 }
 
 export default App;
